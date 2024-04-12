f1149e9dc9d815ddd3ea7303feffa5fede94e510
Srinath Kailasa <srinathkailasa@gmail.com>
2024-04-12 17:11:04 +0100

    Empty target and fft translations

--- kifmm/src/new_fmm/field_translation/target.rs
+++ kifmm/src/new_fmm/field_translation/target.rs
@@ -6,8 +6,9 @@ use green_kernels::{traits::Kernel as KernelTrait, types::EvalType};
 
 use crate::{
     new_fmm::KiFmm,
-    traits::{field::SourceToTargetData as SourceToTargetDataTrait, fmm::TargetTranslation},
+    traits::{field::SourceToTargetData as SourceToTargetDataTrait, fmm::TargetTranslation}
 };
+
 
 impl<Scalar, Kernel, SourceToTargetData> TargetTranslation
     for KiFmm<Scalar, Kernel, SourceToTargetData>
@@ -17,11 +18,17 @@ where
     SourceToTargetData: SourceToTargetDataTrait + Send + Sync,
     <Scalar as RlstScalar>::Real: Default,
 {
-    fn l2l(&self, level: u64) {}
+    fn l2l(&self, level: u64) {
 
-    fn l2p(&self) {}
+    }
 
-    fn p2p(&self) {}
+    fn l2p(&self) {
+
+    }
+
+    fn p2p(&self) {
+
+    }
 
     fn m2p(&self) {}
 }
