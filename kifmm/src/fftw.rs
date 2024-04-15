81561a34e7eeb45d41f290c4c6f48a38c6aa0a6d
Srinath Kailasa <srinathkailasa@gmail.com>
2024-04-15 12:09:36 +0100

    Fix broken doc

--- kifmm/src/fftw.rs
+++ kifmm/src/fftw.rs
@@ -1,12 +1,7 @@
 //! # FFTW Bindings
 //!
 //! A subset of the FFTW library relevant for the kernel independent fast multipole method. Specifically the functionality
-//! for computing real-to-complex transforms on 3D data.
-//!
-//! The bindings are generated
-//! # Features
-//! * The `r2c` and `c2r` transforms implemented on buffers representing 3D data, expected in column major order.
-//! * Optionally parallel `r2c` and `c2r` which share a plan to compute batched transforms of multiple input data sets.
+//! for computing 3D real-to-complex and complex-to-complex DFTs.
 //!
 mod c2c;
 mod dft;
