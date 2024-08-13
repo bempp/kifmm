6a505de7afb24ca2c9818c6b9b14bdc4ceb4a078
Srinath Kailasa <srinathkailasa@gmail.com>
2024-08-13 10:47:32 +0100

    Format

--- kifmm/src/lib.rs
+++ kifmm/src/lib.rs
@@ -92,11 +92,11 @@ pub use fmm::types::BlasFieldTranslationSaRcmp;
 #[doc(inline)]
 pub use fmm::types::FftFieldTranslation;
 #[doc(inline)]
+pub use fmm::types::FmmSvdMode;
+#[doc(inline)]
 pub use fmm::types::SingleNodeBuilder;
 #[doc(inline)]
 pub use fmm::types::SingleNodeFmmTree;
-#[doc(inline)]
-pub use fmm::types::FmmSvdMode;
 
 #[cfg(feature = "mpi")]
 #[doc(inline)]
